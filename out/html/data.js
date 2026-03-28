fb25b1c312969e3b77becd86a64d7f7fab87f7a3
Nightingale-creator <koustubhgalagali00@gmail.com>
2026-03-28 22:26:19 +0530

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -1,1 +1,12 @@
+const tooltipList = [{
+    searchString: "PS",
+    explanationText: "<img src=img/pfjp.png> The People's Freedom amd Justice Party,our Party"
+}];
 
+
+const colourList = [{
+        word: "PFJP",
+        style: "color: #BA0000; font-weight: bold;",
+        img: "img/pfjp.png" // this line is not mandatory
+    }
+];
