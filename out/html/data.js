5e439953c34b4eb80b579c6d69507ac48bab5fde
Nightingale-creator <koustubhgalagali00@gmail.com>
2026-03-29 13:26:40 +0530

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -1,20 +1,64 @@
 const tooltipList = [{
     searchString: "PFJP",
-    explanationText: "<img src=img/pfjp.png> The People's Freedom and Justice Party,our Party."
+    explanationText: "<img src=img/pfjp.png> The People's Freedom and Justice Party,our Liberal Democratic Party."
 },
   {
     searchString: "SLP",
     explanationText: "<img src=img/slp.png> The Sordish Laissez-Faire Party,the Party of industrial interests."
 },
+
+               
+  {
+    searchString: "WPB",
+    explanationText: "<img src=img/wpb.png> The Workers' Party of Bludia,the Party of Bludish People."
+},      
+ 
+  {
+    searchString: "NFP",
+    explanationText: "<img src=img/nfp.png> The National Front Party,The most despicable Party of Racist interests."
+},              
+
+       
+  {
+    searchString: "CPS",
+    explanationText: "<img src=img/cps.png> The Communist Party of Sordland,the catch-all Party of the Left to Far Left."
+},              
+
+      
+  {
+    searchString: "USP",
+    explanationText: "<img src=img/usp.png> The United Sordland Party,the big-tent Establishment Party of the Republic."
+},               
+                     
     ];
 
 
 const colourList = [{
-        word: "PFJP",
+    
+    word: "PFJP",
         style: "color: #24d1f0; font-weight: bold;",
     },
 {
         word: "SLP",
         style: "color: #69A2BE; font-weight: bold;",
 },
+                    {
+    word: "CPS",
+        style: "color: #8B0000; font-weight: bold;",
+    },
+{
+        word: "USP",
+        style: "color: #811aa3; font-weight: bold;",
+},
+                    {
+    word: "WPB",
+        style: "color: #D5AC27; font-weight: bold;",
+    },
+{
+        word: "NFP",
+        style: "color: #954B00; font-weight: bold;",
+},
 ];
+        
+    
+
