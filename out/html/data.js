f54aafe413d25304e36cc0a6f19848e114abd53f
Nightingale-creator <koustubhgalagali00@gmail.com>
2026-03-29 09:53:43 +0530

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -1,11 +1,23 @@
 const tooltipList = [{
     searchString: "PFJP",
-    explanationText: "<img src=img/pfjp.png> The People's Freedom amd Justice Party,our Party"
-}];
+    explanationText: "<img src=img/pfjp.png> The People's Freedom and Justice Party,our Party."
+}
+  const tooltipList = [{
+    searchString: "SLP",
+    explanationText: "<img src=img/slp.png> The Sordish Laissez-Faire Party,the Party of industrial interests."
+}                  
+
+                    
+];
 
 
 const colourList = [{
         word: "PFJP",
         style: "color: #24d1f0; font-weight: bold;",
     }
+const colourList = [{
+        word: "SLP",
+        style: "color: #69A2BE; font-weight: bold;",
+}
+                    
 ];
