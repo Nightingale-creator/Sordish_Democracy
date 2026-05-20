37577519778c448a2628235b04bb98e9b0e48089
Nightingale-creator <koustubhgalagali00@gmail.com>
2026-05-20 10:10:05 +0530

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -214,13 +214,16 @@ function applyWholesome(str) {
   };
 
   // TODO: have some code for tabbed sidebar browsing.
-  window.updateSidebar = function() {
-      $('#qualities').empty();
-      var scene = dendryUI.game.scenes[window.statusTab];
-      dendryUI.dendryEngine._runActions(scene.onArrival);
-      var displayContent = dendryUI.dendryEngine._makeDisplayContent(scene.content, true);
-      $('#qualities').append(dendryUI.contentToHTML.convert(displayContent));
-  };
+      window.updateSidebar = function () {
+        $('#qualities').empty();
+        var statusScene = dendryUI.game.scenes["status"];
+        var scene = dendryUI.game.scenes[window.statusTab];
+        dendryUI.dendryEngine._runActions(statusScene.onArrival);
+        dendryUI.dendryEngine._runActions(scene.onArrival);
+        var displayContent = dendryUI.dendryEngine._makeDisplayContent(scene.content, true);
+        $('#qualities').append(dendryUI.contentToHTML.convert(displayContent));
+        dendryUI.dendryEngine._runActions(scene.onDisplay);
+    };
 
   window.changeTab = function(newTab, tabId) {
       if (tabId == 'poll_tab' && dendryUI.dendryEngine.state.qualities.historical_mode) {
